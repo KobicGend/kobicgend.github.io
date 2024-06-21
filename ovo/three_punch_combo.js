af51bdc3b7c1cedef17a23085aaae1e930b53a58
KobicGend <cmg77@126.com>
2024-06-21 19:32:41 +0800

    Site updated: 2024-06-21 19:32:24

---
+++ ovo/three_punch_combo.js
@@ -0,0 +1,1 @@
+$i=0,$("#start").click(function(){6<=++$i&&($("#start").hide(),$("#stop").show())}),$("#stop").click(function(){alert("这么作？今天别吃了！"),$(this).hide()}),$("#close_banner").click(function(){$("#banner").hide()});
